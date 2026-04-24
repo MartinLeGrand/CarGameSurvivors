a667d82a56bece8e1de1c8f17f10f7b0acec21aa
SergiuszStein <sergiuszstein@gmail.com>
2026-04-24 21:16:19 +0200

    Pushing Project

---
+++ Plugins/BlueprintableInput/Source/BlueprintableInput/Public/BpInputTrigger.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputTriggers.h"
+#include "BpInputTrigger.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class BLUEPRINTABLEINPUT_API UBpInputTrigger : public UInputTrigger
+{
+	GENERATED_BODY()
+	
+};
---
+++ Plugins/BlueprintableInput/Source/BlueprintableInput/Public/BpInputTriggerTimedBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputTriggers.h"
+#include "BpInputTriggerTimedBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class BLUEPRINTABLEINPUT_API UBpInputTriggerTimedBase : public UInputTriggerTimedBase
+{
+	GENERATED_BODY()
+	
+};
